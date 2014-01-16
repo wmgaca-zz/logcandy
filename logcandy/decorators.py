fb98c68f07d4596b1913d26420167d9861bc025a
Wojciech Gaca <wmgaca@gmail.com>
2014-01-16 16:49:26 +0400

    Added a decorator for classes.

--- logcandy/decorators.py
+++ logcandy/decorators.py
@@ -12,8 +12,8 @@ __has_return_statement = {}
 
 
 def __function_has_return_statement(function):
-    """ Helper function, checks if a function given as a parameter
-        has a return statement. Returns boolean."""
+    """Helper function, checks if a function given as a parameter
+    has a return statement. Returns boolean."""
 
     if function not in __has_return_statement:
         pattern = re.compile(r'(^| |:)return($| |\()')
@@ -24,8 +24,8 @@ def __function_has_return_statement(function):
 
 
 def __get_function_parameters(function, *args, **kwargs):
-    """ Helper function, returns a dictionary containing function
-        parameter names and values."""
+    """Helper function, returns a dictionary containing function
+    parameter names and values."""
 
     # Get names and default arguments
     fargspec = inspect.getargspec(function)
@@ -69,6 +69,7 @@ def __format_value(value):
 
 
 def footprint(function):
+    """Decorator logging function's params and return value."""
 
     # Make sure that passed argument is a function
     assert inspect.isfunction(function), 'Cannot decorate: not a function!'
@@ -83,35 +84,40 @@ def footprint(function):
         fargdict = dict(farglist)
 
         if 'self' in fargdict:
-            call_str = 'CALL %s.%s.%s (' % (function.__module__, fargdict['self'].__class__.__name__, function.__name__)
+            call_str = 'CALL %s.%s.%s (' % (function.__module__, 
+                                            fargdict['self'].__class__.__name__, 
+                                            function.__name__)
         elif 'cls' in fargdict:
-            call_str = 'CALL %s.%s.%s (' % (function.__module__, fargdict['cls'].__name__, function.__name__)
+            call_str = 'CALL %s.%s.%s (' % (function.__module__, 
+                                            fargdict['cls'].__name__, 
+                                            function.__name__)
         else:
-            call_str = 'CALL %s.%s (' % (function.__module__, function.__name__)
+            call_str = 'CALL %s.%s (' % (function.__module__, 
+                                         function.__name__)
 
         if not farglist:
             log.info('%s)' % call_str)
         elif len(farglist) == 1:
             log.info('%s%s = %s)' % (call_str,
-                                         farglist[0][0],
-                                         __format_value(farglist[0][1])))
+                                     farglist[0][0],
+                                     __format_value(farglist[0][1])))
         else:
             indent = ' ' * len(call_str)
 
             # First parameter
             log.info('%s%s = %s' % (call_str,
-                                        farglist[0][0],
-                                        __format_value(farglist[0][1])))
+                                    farglist[0][0],
+                                    __format_value(farglist[0][1])))
 
             # [1:-1] parameters
             for name, value in farglist[1:-1]:
                 log.info('%s%s = %s' % (indent, name,
-                                            __format_value(value)))
+                                        __format_value(value)))
 
             # Last parameters
             log.info('%s%s = %s)' % (indent,
-                                         farglist[-1][0],
-                                         __format_value(farglist[-1][1])))
+                                     farglist[-1][0],
+                                     __format_value(farglist[-1][1])))
 
         log.indent()
 
@@ -121,7 +127,7 @@ def footprint(function):
         # Print footer and the return value (if present)
         if __function_has_return_statement(function):
             log.info('END  %s -> %s' % (function.__name__,
-                                            __format_value(result)))
+                                        __format_value(result)))
         else:
             log.info('END  %s' % function.__name__)
 
@@ -134,26 +140,34 @@ def footprint(function):
 
 
 def assert_not_none(function):
+    """Decorator asserting a function's return value is not None."""
+
     def function_wrapper(*args, **kwargs):
         result = function(*args, **kwargs)
         assert result is not None, 'Function %s output is None!' % function.__name__
         return result
     return function_wrapper
 
+
 # Auto decoration flag
 DO_NOT_DECORATE_FLAG = '__do_not_decorate'
 
 
 def disable_auto_decoration(function):
+    """Set a DO_NOT_DECORATE flag to a function."""
+
     setattr(function, DO_NOT_DECORATE_FLAG, True)
     return function
 
 
 def auto_decoration_enabled(function):
+    """Check if auto-decoration is not disabled for a function."""
+
     return inspect.isfunction(function) and not hasattr(function, DO_NOT_DECORATE_FLAG)
 
 
 class _DecorateAllMethods(type):
+    """Metaclass decorating all methods within a class that's using it."""
 
     _decorator_name = None
     _disable = []
@@ -173,12 +187,32 @@ class _DecorateAllMethods(type):
 
 
 class FootprintAllMethods(_DecorateAllMethods):
+    """Metaclass decorating all methods with a footprint decorator."""
 
     _decorator_name = 'footprint'
     _disable = ['__repr__', '__str__']
 
 
-def register_footprint(global_items):
-    for name, value in global_items.items():
-        if isinstance(value, types.FunctionType):
-            print '%s is a function type' % name
+#def register_footprint(global_items):
+#    """Highly experimental."""
+#
+#    for name, value in global_items.items():
+#        if isinstance(value, types.FunctionType):
+#            print '%s is a function type' % name
+
+
+class footprintcls(object):
+    """A decorator for classes. In case your class has already a metaclass."""
+
+    def __init__(self, decorator=None):
+        self.decorator = decorator or footprint
+
+    def __call__(self, cls):
+        for attr_name, attr_value in cls.__dict__.items():
+            if attr_name in FootprintAllMethods._disable:
+                continue
+            if not auto_decoration_enabled(attr_value):
+                continue
+            setattr(cls, attr_name, self.decorator(attr_value))
+        return cls
+
--- logcandy/example.py
+++ logcandy/example.py
@@ -1,8 +1,29 @@
 import log, decorators
-from decorators import FootprintAllMethods, footprint
+from decorators import FootprintAllMethods, footprint, footprintcls
 
 
+@footprintcls()
 class Test(object):
+
+    def __init__(self):
+        self.foo = 'Foo'
+
+    def __str__(self):
+        return 'Test'
+
+    __repr__ = __str__
+
+    def bar(self, hello='World'):
+        self.aj()
+
+    def aj(self):
+        self.waj()
+
+    def waj(self):
+        log.debug('waj!')
+
+
+class Test2(object):
 
     __metaclass__ = FootprintAllMethods
 
@@ -23,14 +44,12 @@ class Test(object):
     def waj(self):
         log.debug('waj!')
 
-
 def main():
     t = Test()
     t.bar(hello='World!!!')
-
-print vars()
-
-decorators.register_footprint(vars())
+    
+    t2 = Test2()
+    t2.bar(hello='World!!!')
 
 if __name__ == '__main__':
     main()
